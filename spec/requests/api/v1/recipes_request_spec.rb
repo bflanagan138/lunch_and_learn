1bb4bf4f2d94ff2b39bca3bc5cd3bf0c8d227134
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-05 11:48:56 -0700

    feat: created fixtures, add places api key to application.yml

--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "recipe API request" do
   it 'returns a list of recipes from provided country' do
-    country = 'Estonia'
-    get "/api/v1/recipes/?q=#{country}"
-   
+    # recipe_response = File.read('spec/fixtures/country_recipes.json')
+    # country_response = File.read('spec/fixtures/country.json')
+    # country = 'Vatican'
+    
+    # stub_request(:get, "https://api.edamam.com/api/recipes/v2?q=#{country}&app_id=#{ENV['edamam_app_id']}&app_key=#{ENV['edamam_app_key']}&type=#{ENV['edamam_type']}")
+    # .to_return(status: 200, body: recipe_response)
+    # stub_request(:get, "https://restcountries.com/v3.1/name/#{country}")
+    # .to_return(status: 200, body: country_response)
+    country = "Vatican"
+    get "/api/v1/recipes?country=#{country}"
+    
     expect(response).to be_successful
     expect(response.status).to be 200
     
@@ -26,7 +34,7 @@ RSpec.describe "recipe API request" do
       expect(recipe[:attributes][:url]).to be_a String
       expect(recipe[:attributes]).to have_key (:country)
       expect(recipe[:attributes][:country]).to be_a String
-      expect(recipe[:attributes][:country]).to eq ("#{country}")
+      expect(recipe[:attributes][:country]).to eq ("#{country}").downcase.capitalize
       expect(recipe[:attributes]).to have_key (:image_url)
       expect(recipe[:attributes][:image_url]).to be_a String
       expect(recipe[:attributes].count).to eq (4)
@@ -36,25 +44,26 @@ RSpec.describe "recipe API request" do
 
   it 'returns an empty array if no country name is provided' do
     country = ''
+   
     get "/api/v1/recipes/?q=#{country}"
-    
     parse = JSON.parse(response.body, symbolize_names: true)
-
+  
     expect(parse).to be_a Hash
     expect(parse).to have_key (:data)
     expect(parse[:data]).to be_a Array
     expect(parse[:data]).to eq ([])
   end
-
-  it 'returns an empty array if no country name is provided' do
+  
+  it 'returns an empty array if non country name is provided' do
     country = 'Dirty Work'
     get "/api/v1/recipes/?q=#{country}"
     
     parse = JSON.parse(response.body, symbolize_names: true)
-
+  
     expect(parse).to be_a Hash
     expect(parse).to have_key (:data)
     expect(parse[:data]).to be_a Array
     expect(parse[:data]).to eq ([])
   end
+  
 end

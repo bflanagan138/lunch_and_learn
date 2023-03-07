48c171c56da936fd9184218f93ea9df5631eca30
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-06 17:59:25 -0700

    refactor: Add webmock to images facade tests, tests passing

--- spec/facades/videos_facade_spec.rb
+++ spec/facades/videos_facade_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe 'videos' do
   it 'returns JSON response' do
     country = 'Estonia'
+    country_response = File.read('spec/fixtures/denmark.json')
+    video_response = File.read('spec/fixtures/denmark_video.json')
+    stub_request(:get, "https://restcountries.com/v3.1/name/#{country}")
+      .to_return(status: 200, body: country_response)
+    stub_request(:get, "https://youtube.googleapis.com/youtube/v3/search?channelId=#{ENV['youtube_channel_id']}&key=#{ENV['google_api_key']}&part=snippet&q=#{country}")
+      .to_return(status: 200, body: video_response)
+      
     video = VideoFacade.video_by_country(country)
     
     expect(video).to be_a Video

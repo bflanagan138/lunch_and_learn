5bf9f52864b6df30d7dfc1b0fcca6705e09f175f
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-06 17:50:21 -0700

    refactor: Add webmock to images service tests, tests passing

--- spec/services/images_service_spec.rb
+++ spec/services/images_service_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 RSpec.describe 'images' do
   it 'returns JSON response' do
     country = 'Estonia'
+    images_response = File.read('spec/fixtures/country_images.json')
+    country_response = File.read('spec/fixtures/country.json')
+    
+    stub_request(:get, "https://api.unsplash.com/search/photos?client_id=#{ENV['unsplash_client_id']}&query=#{country}")
+    .to_return(status: 200, body: images_response)
+    stub_request(:get, "https://restcountries.com/v3.1/name/#{country}")
+      .to_return(status: 200, body: country_response)
+
     results = ImagesService.images_by_country(country)
 
     expect(results).to be_a Hash

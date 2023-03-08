bbfdf35cb08c4aa1d92b0cb4a4d5444a0f3c18e4
Bryan Flanagan <bflanagan78@yahoo.com>
2023-03-07 19:27:46 -0700

    feat/test: Add save recipe with tests, tests passing

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -2,44 +2,43 @@ require 'rails_helper'
 
 RSpec.describe "favorites" do
   it 'saves a recipe as a favorite' do
+    country = "Thailand"
+    link = "https://www.tastingtable.com/687784/crab-fried-rice-recipe-from-chef-bee/"
+    title = "Crab Fried Rice (Khaao Pad Bpu)"
+    user = User.create!({ name: "Robert Smith", email: "rsmith@thecure.com", api_key: SecureRandom.hex(16)})
+    body = { api_key: user.api_key, country: country, recipe_link: link, recipe_title: title}
+    headers = { "Content-Type": "application/json", "Accept": "application/json" }
+    
+    post "/api/v1/favorites", headers: headers, params: JSON.generate(body)
+   
+    expect(response).to be_successful
+    expect(response.status).to eq 201
+
+    parse = JSON.parse(response.body, symbolize_names: true)
+   
+    expect(parse).to be_a Hash
+    expect(parse).to have_key (:success)
+    expect(parse[:success]).to eq ("Favorite added successfully")
+  end
+
+  it 'returns an error if api key is invalid' do
     
     country = "Thailand"
     link = "https://www.tastingtable.com/687784/crab-fried-rice-recipe-from-chef-bee/"
     title = "Crab Fried Rice (Khaao Pad Bpu)"
     user = User.last
-    body = { api_key: "#{user.api_key}", country: country, recipe_link: link, recipe_title: title}
+    body = { api_key: "abcd", country: country, recipe_link: link, recipe_title: title}
     headers = { "Content-Type": "application/json", "Accept": "application/json" }
  
     post "/api/v1/favorites", headers: headers, params: JSON.generate(body)
-    
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq 401
+
+    parse = JSON.parse(response.body, symbolize_names: true)
+   
+    expect(parse).to be_a Hash
+    expect(parse).to have_key (:error)
+    expect(parse[:error]).to eq ("Login credentials missing or invalid")
   end
 end
-#     expect(response).to be_successful
-#     expect(response.status).to be 201
-#     parse = JSON.parse(response.body, symbolize_names: true)
-# require 'pry'; binding.pry
-#     expect(parse).to be_a Hash
-#     expect(parse).to have_key (:data)
-#     expect(parse[:data]).to be_a Hash
-#     expect(parse[:data]).to have_key (:id)
-#     expect(parse[:data][:id]).to be_a String
-#     expect(parse[:data]).to have_key (:type)
-#     expect(parse[:data][:type]).to be_a String
-#     expect(parse[:data]).to have_key (:attributes)
-#     expect(parse[:data][:attributes]).to be_a Hash
-#     expect(parse[:data][:attributes]).to have_key (:name)
-#     expect(parse[:data][:attributes][:name]).to be_a String
-#     expect(parse[:data][:attributes]).to have_key (:email)
-#     expect(parse[:data][:attributes][:email]).to be_a String
-#     expect(parse[:data][:attributes]).to have_key (:api_key)
-#     expect(parse[:data][:attributes][:api_key]).to be_a String
-
-#     new_user = User.last
-#     expect(new_user).to be_a User
-#     expect(new_user.id).to eq User.last[:id]
-#     expect(new_user.id).to be_a Integer
-#     expect(new_user.name).to be_a String
-#     expect(new_user.name).to eq(body[:name])
-#     expect(new_user.email).to be_a String
-#     expect(new_user.email).to eq(body[:email])
-#     expect(new_user.api_key).to be_a String
